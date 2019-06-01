bef71d10933e747622298501be7b3c082e53ca87
Tony Rubio <antwon510@yahoo.com>
2019-06-01 11:09:55 -0400

    commit

--- GUI/test/tonyGUI.py
+++ GUI/test/tonyGUI.py
@@ -13,30 +13,31 @@ desDir9 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib'))
 
 sys.path.extend([desDir1,desDir2,desDir3,desDir4,desDir5,desDir6,desDir7,desDir8,desDir9])
 
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5 import QtGui
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.title = "iMay - AI in Your Hands"
-        self.iconPath = "home.jpg"
-        self.top = 100
-        self.left = 100
-        self.width = 400
-        self.height = 300
+        title = "iMay - AI in Your Hands"
+        iconPath = "home.jpg"
+        top = 100
+        left = 100
+        width = 400
+        height = 300
 
-        self.InitWindow()
-
-    def InitWindow(self):
-        self.setWindowIcon(QtGui.QIcon(self.iconPath))
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowTitle(title)
+        self.setWindowIcon(QtGui.QIcon(iconPath))
+        self.setGeometry(left, top, width, height)
 
         self.show()
 
-App = QApplication(sys.argv)
-window = Window()
-sys.exit(App.exec())
+
+
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec())
 
