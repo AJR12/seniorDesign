1c9dc6b8c13c217e6f22fd27a3a3d9a84dc944c4
AJR12 <arubi010@fiu.edu>
2019-06-22 18:33:27 -0400

    commit

--- GUI/bin/IMayFinal.py
+++ GUI/bin/IMayFinal.py
@@ -5,6 +5,20 @@
 # Created by: PyQt5 UI code generator 5.12.1
 #
 # WARNING! All changes made in this file will be lost!
+import sys
+import os
+curDir = os.getcwd()
+desDir1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+desDir2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv'))
+desDir3 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv/Scripts/python36.zip'))
+desDir4 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv/lib'))
+desDir5 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv/lib/site-packages'))
+desDir6 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv/lib/site-packages/setuptools-40.8.0-py3.6.egg'))
+desDir7 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../venv/lib/site-packages/pip-19.0.3-py3.6.egg'))
+desDir8 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+desDir9 = os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib'))
+
+sys.path.extend([desDir1,desDir2,desDir3,desDir4,desDir5,desDir6,desDir7,desDir8,desDir9])
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from GUI.lib import IMayFunctions
